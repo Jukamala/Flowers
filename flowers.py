6cf7c8c9c90594b283a88ad767a6a3b73fd122ba
Justus Will <justuswill@gmail.com>
2020-04-27 09:21:16 +0200

    Generic colors

--- flowers.py
+++ flowers.py
@@ -57,31 +57,35 @@ def get_color(fls, typ):
 
 class Flower:
 
-    def __init__(self, typ, genes, step=0, power=0, tests=0, parent_genes=None, testing=None, p=None):
-        self.p = p
+    def __init__(self, typ, genes, step=0, power=0, tests=0, parent_genes=None, testing=None, p=None, generic=None):
         self.typ = typ
         self.genes = genes
         self.step = step
         self.power = power
         self.tests = tests
         self.parent_genes = parent_genes
+        self.p = p
+        self.generic = generic
         self.color_dict = {0: 'white', 1: 'black', 2: 'pink', 3: 'purple', 4: 'red',
                            5: 'yellow', 6: 'orange', 7: 'blue', 8: 'green'}
         self.test_seed, self.ac_cols, self.rej_cols, self.alt = testing or 4 * [None]
 
     def __repr__(self):
-        return "<%s | %s | %d %d (%d)>" % (self.genes, self.color_dict[int(get_color(self.genes, self.typ))],
-                                           self.step, self.power, self.tests)
+        genes = self.genes or "Generic"
+        col = self.color_dict[self.generic if self.generic is not None else int(get_color(self.genes, self.typ))]
+        return "<%s | %s | %d %d (%d)>" % (genes, col, self.step, self.power, self.tests)
 
     def prnt(self):
+        genes = self.genes or "Generic"
+        gene_str = "%10s" % str(genes) if self.typ != 'rose' else "%13s" % str(genes)
+        col = self.color_dict[self.generic if self.generic is not None else int(get_color(self.genes, self.typ))]
+        test_str = "" if self.test_seed is None else " [alt:%s breed with %s for %s%s]" %\
+                                                     (self.alt, self.test_seed, self.ac_cols,
+                                                      "" if len(self.rej_cols) == 0 else " not %s" % self.rej_cols)
+        chance_str = "" if self.p is None else " [%s %%]" % (int(100 * self.p) if (100 * self.p) % 1 < 0.01 else
+                                                             "%.1f" % (100 * self.p))
         return "<%s | %6s | %d %2d <- %s%s%s>" %\
-               (self.genes, self.color_dict[int(get_color(self.genes, self.typ))], self.step, self.power,
-                self.parent_genes or 'Seed',
-                "" if self.test_seed is None else " [alt:%s breed with %s for %s%s]" %
-                                                  (self.alt, self.test_seed, self.ac_cols,
-                                                   "" if len(self.rej_cols) == 0 else " not %s" % self.rej_cols),
-                "" if self.p is None else " [%s %%]" % (int(100 * self.p) if (100 * self.p) % 1 < 0.01 else
-                                                        "%.1f" % (100 * self.p)))
+               (gene_str, col, self.step, self.power, self.parent_genes or 'Seed', test_str, chance_str)
 
 
 class Breeder:
@@ -140,7 +144,7 @@ class Breeder:
         seen = set()
         fls = set()
         for fl in sorted(self.pool.values(), key=lambda fl: fl.power):
-            fl_c = int(get_color(fl, self.typ))
+            fl_c = int(fl.generic if fl.generic is not None else get_color(fl, self.typ))
             if fl_c not in seen:
                 seen.add(fl_c)
                 fls.add(fl)
@@ -178,7 +182,7 @@ class Breeder:
     def step(self):
         """
         Performs a breeding step on the pool of flowers currently available.
-        Breed all pairs of available flowers.
+        Breed all pairs of available flowers with known genes.
         Take new flowers in to the pool and update flowers when better origins are found.
         Output:
         Returns if something was changed in this step.
@@ -188,15 +192,15 @@ class Breeder:
         new_pool = []
         for i in range(len(pool_fls)):
             for j in range(i, len(pool_fls)):
-                new_pool += self.breed(pool_fls[i], pool_fls[j])
-
+                if pool_fls[i].genes is not None and pool_fls[j].genes:
+                    new_pool += self.breed(pool_fls[i], pool_fls[j])
         for fl in new_pool:
-            if fl.genes not in self.pool:
-                self.pool[fl.genes] = fl
+            if (fl.genes or fl.generic) not in self.pool:
+                self.pool[fl.genes or fl.generic] = fl
                 change = True
             else:
-                if self.comp(fl, self.pool[fl.genes]):
-                    self.pool[fl.genes] = fl
+                if self.comp(fl, self.pool[fl.genes or fl.generic]):
+                    self.pool[fl.genes or fl.generic] = fl
                     change = True
         return change
 
@@ -226,6 +230,17 @@ class Breeder:
                       power=fl1.power + (fl2.power if fl1.genes != fl2.genes else 0) + 1/pr,
                       parent_genes=(fl1.genes, fl2.genes), tests=fl1.tests + fl2.tests, p=pr)
                for ch, pr in zip(childs, probs) if get_color(tuple(ch), self.typ) in unique_cols]
+        many_cols = [c for c in cnts if cnts[c] >= 1]
+
+        # Generic colors
+        col_chances = dict()
+        for cc, p_ in zip(cols, probs):
+            col_chances[cc] = col_chances.get(cc, 0) + p_
+
+        fls += [Flower(fl1.typ, genes=None, step=max(fl1.step, fl2.step) + 1,
+                       power=fl1.power + (fl2.power if fl1.genes != fl2.genes else 0) + 1 / pr,
+                       parent_genes=(fl1.genes, fl2.genes), tests=fl1.tests + fl2.tests, p=pr, generic=cc)
+                for cc, pr in col_chances.items() if cc in many_cols]
 
         # If testing is allowed
         if self.test >= 1:
@@ -307,7 +322,12 @@ class Breeder:
                                    power=(fl1.power + (fl2.power if fl1.genes != fl2.genes else 0) +
                                           test_seed.power + 1/pr1 + (1 + pr2/pr1) * level),
                                    tests=fl1.tests + fl2.tests + test_seed.tests + 1, p=pr1,
-                                   testing=[test_seed.genes, ac_cols, rej_cols, ch2])]
+                                   testing=[test_seed.genes, ac_cols, rej_cols, tuple(ch2)])]
+                else:
+                    fls += [Flower(fl1.typ, genes=None, step=max(fl1.step, fl2.step) + 1,
+                                   power=fl1.power + (fl2.power if fl1.genes != fl2.genes else 0) + 1 / (pr1 + pr2),
+                                   parent_genes=(fl1.genes, fl2.genes), tests=fl1.tests + fl2.tests, p=pr1 + pr2,
+                                   generic=int(get_color(tuple(ch1), typ=self.typ)))]
 
         return fls
 
@@ -396,9 +416,20 @@ seeds = {'rose': [Flower('rose', (0, 0, 1, 0)), Flower('rose', (0, 2, 2, 0)), Fl
          'windflower': [Flower('windflower', (0, 0, 1)), Flower('windflower', (0, 2, 2)),
                         Flower('windflower', (2, 0, 2))]}
 
+island_seeds = {'rose': [Flower('rose', (0, 0, 1, 0)), Flower('rose', (0, 2, 2, 0)), Flower('rose', (2, 0, 2, 1))],
+         'cosmos': [Flower('cosmos', (0, 0, 1)), Flower('cosmos', (0, 2, 1)), Flower('cosmos', (2, 0, 0))],
+         'lilly': [Flower('lilly', (0, 0, 2)), Flower('lilly', (0, 2, 0)), Flower('lilly', (2, 0, 1))],
+         'pansy': [Flower('pansy', (0, 0, 1)), Flower('pansy', (0, 2, 2)), Flower('pansy', (2, 0, 2))],
+         'hyacinth': [Flower('hyacinth', (0, 0, 1)), Flower('hyacinth', (0, 2, 2)), Flower('hyacinth', (2, 0, 1))],
+         'tulip': [Flower('tulip', (0, 0, 1)), Flower('tulip', (0, 2, 0)), Flower('tulip', (2, 0, 1))],
+         'mum': [Flower('mum', (0, 0, 1)), Flower('mum', (0, 2, 2)), Flower('mum', (2, 0, 2))],
+         'windflower': [Flower('windflower', (0, 0, 1)), Flower('windflower', (0, 2, 2)),
+                        Flower('windflower', (2, 0, 2))]}
+
 
 if __name__ == '__main__':
 
+    # Optimal routes with testing
     for typ in seeds.keys():
         print(typ)
         br = Breeder(typ=typ, test=3)
@@ -408,20 +439,12 @@ if __name__ == '__main__':
 
     print('~~~~~')
 
+    # Optimal routes without testing
     for typ in seeds.keys():
         print(typ)
         br = Breeder(typ=typ, test=0)
         br.all_pos()
         br.result()
-        print('*****')
-
-    print('~~~~~')
-
-    for typ in seeds.keys():
-        print(typ)
-        br = Breeder(typ=typ, test=3)
-        br.all_pos()
-        br.result(big=True)
         print('*****')
 
     print('~~~~~')
